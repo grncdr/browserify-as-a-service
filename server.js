81e9354b372489fea5500b59a05167c937a370d8
Stephen Sugden <me@stephensugden.com>
2013-06-16 17:28:06 -0700

    serve static bundles

--- server.js
+++ server.js
@@ -2,38 +2,49 @@
  * This server is just a demo, discussion of the API to be exposed can be found
  * here: https://github.com/grncdr/browserify-as-a-service/issues/3
  */
-var http = require('http');
-var bundleFactory  = require('browserify-bundle-factory');
+var http          = require('http');
+var bundleFactory = require('browserify-bundle-factory');
+var ecstatic      = require('ecstatic');
 
 var getBundle = bundleFactory({
   cache: __dirname + '/package-cache',
   bundleDir: __dirname + '/public/bundles'
 });
 
+var serveStatic = ecstatic({
+  root:     __dirname + '/public',
+  showDir: true,
+  gzip:    true
+});
+
 http.createServer(handleRequest).listen(process.env.PORT || 8888);
 
 function handleRequest(req, res) {
-  // just going to go on the internet and tell lies
   res.setHeader('Content-Type', 'application/json');
+
   function fail(code, message) {
     res.writeHead(code, message || http.STATUS_CODES[code]);
     res.end(JSON.stringify({message: message}));
   }
-  switch (req.method) {
-  case 'POST':
-    if (req.url != '/browserify') {
-      fail(404);
-    } else {
-      sendBundle(req, res);
+
+  serveStatic(req, res, function (err) {
+    if (err) {
+      res.write(err.stack);
+      return fail(500);
     }
-    break;
-  case 'GET':
-    fail(404)
-    // TODO static file serving, especially cached bundles
-    break;
-  default:
-    fail(405);
-  }
+
+    switch (req.method) {
+    case 'POST':
+      if (req.url != '/browserify') {
+        fail(404);
+      } else {
+        sendBundle(req, res);
+      }
+      break;
+    default:
+      fail(405);
+    }
+  })
 }
 
 function sendBundle(req, res) {
